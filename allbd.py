d74ac53c6b8317335870510be8cc500105830923
bihari-dalla <firstyeardatascience@gmail.com>
2026-03-31 14:47:59 +0530

    allbd.py

---
+++ allbd.py
@@ -0,0 +1,14 @@
+'''
+all BIG DATA
+p1 : https://tinyurl.com/bd-mapreduce (word count)
+p2 : https://tinyurl.com/bd-operations
+p3(1) : https://tinyurl.com/bd-decisiontree
+p3(2) : https://tinyurl.com/bd-svmtree
+p4 : https://tinyurl.com/bd-sparkcount (word count)
+p5 : https://tinyurl.com/bd-histogram (movie rating spark)
+p6  : https://tinyurl.com/bd-popularmovie (spark)
+p7 : https://tinyurl.com/bd-sparktree (real estate)(decision tree)
+p8 : https://tinyurl.com/bd-randomforest (one million ratings)
+p9 : https://tinyurl.com/bd-linearregression (linear regression, fabricated, spark)
+p10 : https://tinyurl.com/bd-sparkml  (produce movie recommendations)
+'''
